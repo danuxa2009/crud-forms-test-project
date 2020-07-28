6e344648db2fa1223b78890d406a5c12f7ba4a7b
Danil Titenko <danuxa2009@gmail.com>
2020-07-28 16:30:43 +0300

    Updates

---
+++ precache-manifest.942357cb797e4ddc9c459e30359e93fd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63ff766b4fd71fc1d4a7f3142b8f6b95",
+    "url": "/crud-forms-test-project/index.html"
+  },
+  {
+    "revision": "d1782751bce1255538ef",
+    "url": "/crud-forms-test-project/static/css/main.be3039e4.chunk.css"
+  },
+  {
+    "revision": "07ad7de1415cd5c27931",
+    "url": "/crud-forms-test-project/static/js/2.4316b9c8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/crud-forms-test-project/static/js/2.4316b9c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d1782751bce1255538ef",
+    "url": "/crud-forms-test-project/static/js/main.e7bd56eb.chunk.js"
+  },
+  {
+    "revision": "5a47dc462a8192904a9d",
+    "url": "/crud-forms-test-project/static/js/runtime-main.48e430be.js"
+  }
+]);
